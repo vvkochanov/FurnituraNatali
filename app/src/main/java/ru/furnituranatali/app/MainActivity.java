827af7d4e5c74d418bbd68ba75e0f94c009c03b0
Vavan <vavant@yandex.ru>
2015-09-20 18:16:45 +0300

    Create MainActivity and layout

---
+++ app/src/main/java/ru/furnituranatali/app/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.furnituranatali.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vavan on 20.09.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
