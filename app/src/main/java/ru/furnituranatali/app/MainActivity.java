4e32b412261390c9ccc47e737140ba8ec2a93608
Vavan <vavant@yandex.ru>
2015-09-21 15:37:47 +0300

    Add toolbar and right icons "more"

--- app/src/main/java/ru/furnituranatali/app/MainActivity.java
+++ app/src/main/java/ru/furnituranatali/app/MainActivity.java
@@ -2,15 +2,35 @@ package ru.furnituranatali.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Vavan on 20.09.2015.
  */
 public class MainActivity extends Activity {
 
+    private Toolbar main_toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitToolbar();
     }
+private void InitToolbar() {
+    main_toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+    main_toolbar.setTitle(R.string.app_name);
+    main_toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            return false;
+        }
+    });
+
+    main_toolbar.inflateMenu(R.menu.toolbar_menu);
 }
+
+}
+
