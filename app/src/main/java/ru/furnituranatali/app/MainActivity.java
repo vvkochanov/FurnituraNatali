de5f2c66282b02fab068c0df07f3829db0c414e9
Vavan <vavant@yandex.ru>
2015-09-21 23:45:32 +0300

    [FNP-5]: доработан ToolBar с учетом рекомендаций MaterialDesign и создан NavigationDrawer (с левой стороны экрана)

--- app/src/main/java/ru/furnituranatali/app/MainActivity.java
+++ app/src/main/java/ru/furnituranatali/app/MainActivity.java
@@ -1,19 +1,20 @@
 package ru.furnituranatali.app;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 /**
  * Created by Vavan on 20.09.2015.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar main_toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -30,6 +31,7 @@ private void InitToolbar() {
     });
 
     main_toolbar.inflateMenu(R.menu.toolbar_menu);
+
 }
 
 }
