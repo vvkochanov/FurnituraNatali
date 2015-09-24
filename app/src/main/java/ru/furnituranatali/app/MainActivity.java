4b6228f6a46c1f39e5cc5b80164999efbabbb75f
Vavan <vavant@yandex.ru>
2015-09-24 18:20:17 +0300

    [FNP-7]: в NavigationDrawer добавлены пара пунктов и сделаны пункты меню справа, исправлены ошибки

--- app/src/main/java/ru/furnituranatali/app/MainActivity.java
+++ app/src/main/java/ru/furnituranatali/app/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.furnituranatali.app;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -10,17 +11,21 @@ import android.view.MenuItem;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MAIN_LAYOUT = R.layout.activity_main;
     private Toolbar main_toolbar;
+    private DrawerLayout  drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(MAIN_LAYOUT);
 
         InitToolbar();
+        InitNavView();
     }
-private void InitToolbar() {
+
+    private void InitToolbar() {
     main_toolbar = (Toolbar) findViewById(R.id.main_toolbar);
     main_toolbar.setTitle(R.string.app_name);
     main_toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
@@ -33,6 +38,11 @@ private void InitToolbar() {
     main_toolbar.inflateMenu(R.menu.toolbar_menu);
 
 }
+
+    private void InitNavView() {
+        drawerLayout =(DrawerLayout) findViewById(R.id.drawer_layout);
+
+    }
 
 }
 
